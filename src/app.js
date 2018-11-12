bdd9b77543dd463e67cd877367f048c45766bafe
punsay <43162782+punsay@users.noreply.github.com>
2018-11-12 20:18:10 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>This is for my lovely friend srishh</div>
+        )
+    }
+}
+
+export default App;
